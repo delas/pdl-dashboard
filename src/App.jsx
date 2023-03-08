bdfed84c47f0c7b584924a18ba7a106c29daec2b
GodOfAllBeings <s174871@student.dtu.dk>
2023-03-08 11:10:07 +0100

    Ready to move the ping to the top level component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
 import {useState, useEffect} from 'react';
 import Home from './Pages/Home/Home';
-// import AddFilePopup from './Components/Popup/AddfilePopup/AddfilePopup';
 import Page1 from './Pages/Page1/Page1';
 import Page2 from './Pages/Page2/Page2';
+import {PingServiceRegistry} from './Services/ServiceRegistryServices';
+import {PingMiner} from './Services/MinerServices';
+import {PingRepository} from './Services/RepositoryServices';
 
 function App(props) {
     const [isLoading, setIsLoading] = useState(true);
@@ -12,7 +14,10 @@ function App(props) {
     const [filePopupOpen, setFilePopupOpen] = useState(false);
     const [actionPopupOpen, setActionPopupOpen] = useState(false);
     const [newHostPopupOpen, setNewHostPopupOpen] = useState(false);
-    const [newHostFromSROpen, setNewHostFromSRPopupOpen] = useState(true);
+    const [newHostFromSROpen, setNewHostFromSRPopupOpen] = useState(false);
+
+    const [allHostStatus, setAllHostStatus] = useState([{}]); // list of objects {id, status}
+    const [isPingActiveForHosts, setIsPingActiveForHosts] = useState([{}]); // List of hosts being actively pinged {id, active}
 
     const toggleSidebar = () => {
         setSidebarOpen(!sidebarOpen);
@@ -38,9 +43,82 @@ function App(props) {
         setNewHostFromSRPopupOpen(!newHostFromSROpen)
     }
 
+    const addHostStatus = (id, status) => {
+        setAllHostStatus(allHostStatus.concat({id: id, status: status}));
+    }
+
+    const removeHostStatus = (id) => {
+        setAllHostStatus(allHostStatus.filter((host) => host.id !== id));
+    }
+
+    const getHostStatus = (id) => {
+        return allHostStatus.filter((host) => host.id === id);
+    }
+
     useEffect(() => {
         setIsLoading(false);
     });
+
+    const getResponseStatus = (res) => {
+        return res.status === 200 && res.data.toUpperCase() === "PONG";
+    }
+
+    const setStatus = (status, id) => {
+        removeHostStatus(id);
+        addHostStatus(id, status);
+    }
+
+    const pingIndividualHost = (host, ping) => {
+        ping(host.hostName).then((res) => {
+            setStatus(getResponseStatus(res) ? "online" : "offline");
+        }).catch((e) => {
+            setStatus('offline');
+            setIsLoading(false);
+        })
+    }
+
+    const isBeingActivelyPinged = (id) => {
+        return isPingActiveForHosts.filter((pingHost) => pingHost.id === id) !== [];
+    }
+
+    const pingOnInterval = async (host, ping, timeBetweenTicks) => {
+        addActivelyPing(host.id, true);
+        setInterval(() => {
+            pingIndividualHost(host, ping)
+        }, timeBetweenTicks);
+    }
+
+    const getPingFunction = (serviceType) => {
+        switch(serviceType){
+            case "miner": return PingMiner;
+            case "repository": return PingRepository;
+            case "service registry": return PingServiceRegistry;
+            default: return null;
+        }
+    }
+
+    useEffect(() => {
+        allHostStatus.forEach((host, index) => {
+            if(!isBeingActivelyPinged(host.id)){
+                pingOnInterval(host, getPingFunction(host.type), 5000);
+            }
+        })
+    }, [isBeingActivelyPinged, allHostStatus]);
+
+    
+
+    const addActivelyPing = (id, active) => {
+        const newPing = {id: id, active: active}; // new object to be added
+        const pingHostObject = isPingActiveForHosts.filter((pingHost) => pingHost.id === id);
+        if(pingHostObject === undefined || pingHostObject === null){ // add new if not already present
+            setIsPingActiveForHosts(isPingActiveForHosts.concat(newPing));
+        } else { //override if present
+            const listWithoutPingHost = isPingActiveForHosts.filter((pingHost) => pingHost.id !== id);
+            setIsPingActiveForHosts(listWithoutPingHost.concat(newPing));
+        }
+    }
+
+    
 
     if(isLoading){
         return (
--- src/Services/MinerServices.js
+++ src/Services/MinerServices.js
@@ -4,3 +4,13 @@ export async function PingMiner(hostname) {
     const urlExtension = "/system/Ping";
     return axios.get(`${hostname}${urlExtension}`);
 }
+
+export async function PostMiner(hostname){
+    const urlExtension = "/miners";
+    return axios.post(`${hostname}${urlExtension}`);
+}
+
+export async function GetMinerConfig(hostname){
+    const urlExtension = "/configurations";
+    return axios.get(`${hostname}${urlExtension}`);
+}
