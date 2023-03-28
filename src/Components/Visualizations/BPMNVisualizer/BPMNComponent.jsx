12a61ab27dfccc5bfbcdbed0e24ae065f6499145
GodOfAllBeings <s174871@student.dtu.dk>
2023-03-28 09:47:37 +0200

    Cleaned up the BPMN visualizer

--- src/Components/Visualizations/BPMNVisualizer/BPMNComponent.jsx
+++ src/Components/Visualizations/BPMNVisualizer/BPMNComponent.jsx
@@ -39,39 +39,19 @@ class BPMNComponent extends React.Component {
     // import function
     function importXML(xml, Viewer) {
       // import diagram
-        Viewer.importXML(xml, function(err) {
-            if (err) {
-                return console.error("could not import BPMN 2.0 diagram", err);
-            }
-
-            var canvas = Viewer.get("canvas"),
-            overlays = Viewer.get("overlays");
-
-            // zoom to fit full viewport
-            canvas.zoom("fit-viewport");
-
-            // attach an overlay to a node
-            overlays.add("SCAN_OK", "note", {
-                position: {
-                    bottom: 0,
-                    right: 0
-                },
-                html: '<div class="diagram-note">Mixed up the labels?</div>'
+        Viewer.importXML(xml)
+            .then(() => {
+                var canvas = Viewer.get("canvas");
+                canvas.zoom("fit-viewport");
+            })
+            .catch(err => {
+                console.error("could not import BPMN 2.0 diagram", err);
             });
-
-            // add marker
-            canvas.addMarker("SCAN_OK", "needs-discussion");
-        });
     }
-
-    // a diagram to display
-    //
-    // see index-async.js on how to load the diagram asynchronously from a url.
-    // (requires a running webserver)
     var diagramXML = getFileContent(this.props.file);
 
     // import xml
-    importXML(diagramXML, this.viewer);
+    if(diagramXML) importXML(diagramXML, this.viewer);
   }
 }
 
